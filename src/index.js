05584ffee466159f7e35c91ec0fcc60dd7882b9e
Raine Virta <raine.virta@gmail.com>
2015-09-28 00:59:47 +0300

    use local ramda version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const linter = require('eslint').linter;
-const { has, test, reduce, always, T, cond, propEq, match, nth, pipe, useWith, identity, allPass, prop, lens, split, join, over, curry, adjust, findIndex, either, __ } = require('ramda');
+const path = require('path');
+const { has, test, reduce, always, T, cond, propEq, match, nth, pipe, allPass, prop, lens, split, join, over, curry, adjust, findIndex, either, __, invoker, prepend, apply } = require('ramda');
 const removeFromObjDstr = require('./remove-from-obj-dstr');
 const addToObjDstr = require('./add-to-obj-dstr');
 const readFileStdin = require('read-file-stdin');
@@ -17,34 +18,40 @@ const ESLINT_OPTS = {
   }
 };
 
-const isRamdaProperty = has(__, require('ramda'));
+//    lineImportsRamda :: String -> Boolean
 const lineImportsRamda =
   either(test(/require\(['"]ramda['"]\)/),
          test(/from ['"]ramda['"]/));
+
+//    parseName :: String -> String
 const parseName = pipe( match(/^"([^"]*?)"/), nth(1) );
-const ruleEq = useWith(propEq('ruleId'), identity);
+
+//    ruleEq :: String -> Object -> Boolean
+const ruleEq = propEq('ruleId');
 
 const lines = split('\n');
 const unlines = join('\n');
 const lineLens = lens(lines, unlines);
 const adjustLine = curry((fn, n, str) =>
   over(lineLens, adjust(fn, n), str));
-const messageContainsRamdaFn =
-  pipe(prop('message'), parseName, isRamdaProperty);
 
-const handleEslintMessage = (code, message) =>
-  cond([
+//    handleEslintMessage :: Object -> String -> Object -> String
+const handleEslintMessage = curry((ramda, code, message) => {
+  const containsRamdaProp =
+    pipe(prop('message'), parseName, has(__, ramda));
+
+  return cond([
     [ allPass([
       ruleEq('no-unused-vars'),
       pipe(prop('source'), lineImportsRamda),
-      messageContainsRamdaFn
+      containsRamdaProp
     ]), (message) => {
       const name = parseName(message.message);
       return adjustLine(removeFromObjDstr(name), message.line - 1, code);
     }],
     [ allPass([
       ruleEq('no-undef'),
-      messageContainsRamdaFn
+      containsRamdaProp
     ]), (message) => {
       const ramdaImportLine = findIndex(lineImportsRamda, lines(code));
       const name = parseName(message.message);
@@ -52,15 +59,36 @@ const handleEslintMessage = (code, message) =>
     } ],
     [ T, always(code) ]
   ])(message);
+});
 
-const handleEslintOutput = (messages, code) =>
-  reduce(handleEslintMessage, code, messages);
+//    handleEslintOutput :: Object -> [Object] -> String
+const handleEslintOutput = (ramda, messages, code) =>
+  reduce(handleEslintMessage(ramda), code, messages);
 
+
+//    fallback :: a -> (b -> a) -> b -> a
+const fallback = curry((def, fn, val) => {
+  try {
+    return fn(val);
+  } catch(e) {
+    return def;
+  }
+});
+
+//    resolveRamda :: process -> Object
+const resolveRamda =
+  pipe(invoker(0, 'cwd'),
+       prepend(__, [ 'node_modules', 'ramda' ]),
+       apply(path.join),
+       fallback(require('ramda'), require));
+
+//    main :: Process -> ()
 const main = (process) => {
   readFileStdin(process.argv[2], (err, buf) => {
+    const localRamda = resolveRamda(process);
     const input = buf.toString();
     const messages = linter.verify(input, ESLINT_OPTS);
-    const output = handleEslintOutput(messages, input);
+    const output = handleEslintOutput(localRamda, messages, input);
     process.stdout.write(output);
   });
 };
