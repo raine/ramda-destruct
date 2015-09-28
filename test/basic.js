2d5a519e327748217cefb209c458a1184978683b
Raine Virta <raine.virta@gmail.com>
2015-09-28 20:32:24 +0300

    write tests with a helper

--- test/basic.js
+++ test/basic.js
@@ -1,84 +1,48 @@
 const removeFromObjDstr = require('../src/remove-from-obj-dstr');
 const addToObjDstr = require('../src/add-to-obj-dstr');
+const { curry, inc, init, last } = require('ramda');
 
-const testRemoveFromObjDstr = (name, input, expected) => {
-  assert.equal(
-    removeFromObjDstr(name, input),
-    expected
-  );
-}
-
-const testAddToObjDstr = (name, input, expected) => {
-  assert.equal(
-    addToObjDstr(name, input),
-    expected
-  );
-}
+const testFn = curry((fn, cases) =>
+  cases.forEach((c, i) => {
+    const args = init(c);
+    const expected = last(c);
+    it(`case ${inc(i)}`, () =>
+      assert.equal(
+        fn(...args),
+        expected
+      ));
+  }));
 
 describe('removeFromObjDstr', () => {
-  it('1', () => {
-    testRemoveFromObjDstr(
-      'map',
+  testFn(removeFromObjDstr, [
+    [ 'map',
       `const { map, filter } = require('ramda');`,
-      `const { filter } = require('ramda');`
-    );
-  });
-
-  it('2', () => {
-    testRemoveFromObjDstr(
-      'filter',
+      `const { filter } = require('ramda');` ],
+    [ 'filter',
       `const { map, filter } = require('ramda');`,
-      `const { map } = require('ramda');`
-    );
-  });
-
-  it('3', () => {
-    testRemoveFromObjDstr(
-      'map',
+      `const { map } = require('ramda');` ],
+    [ 'map',
       `const { map } = require('ramda');`,
-      `const {  } = require('ramda');`
-    );
-  });
-
-  it('4', () => {
-    testRemoveFromObjDstr(
-      'bar',
+      `const {  } = require('ramda');` ],
+    [ 'bar',
       `const { foo, bar, xyz } = require('ramda');`,
-      `const { foo, xyz } = require('ramda');`
-    );
-  });
+      `const { foo, xyz } = require('ramda');` ]
+  ]);
 });
 
 describe('addToObjDstr', () => {
-  it('1', () => {
-    testAddToObjDstr(
-      'map',
+  testFn(addToObjDstr, [
+    [ 'map',
       `const { filter, reduce } = require('ramda');`,
-      `const { filter, reduce, map } = require('ramda');`
-    );
-  });
-
-  it('2', () => {
-    testAddToObjDstr(
-      'map',
+      `const { filter, reduce, map } = require('ramda');` ],
+    [ 'map',
       `const { } = require('ramda');`,
-      `const { map } = require('ramda');`
-    );
-  });
-
-  it('3', () => {
-    testAddToObjDstr(
-      'map',
+      `const { map } = require('ramda');` ],
+    [ 'map',
       `const {} = require('ramda');`,
-      `const {map} = require('ramda');`
-    );
-  });
-
-  it('4', () => {
-    testAddToObjDstr(
-      'filter',
+      `const {map} = require('ramda');` ],
+    [ 'filter',
       `const {map} = require('ramda');`,
-      `const {map, filter} = require('ramda');`
-    );
-  });
+      `const {map, filter} = require('ramda');` ]
+  ]);
 });
