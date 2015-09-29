e0ead6156ae00d13174272b01641ebb7b6ee7713
Raine Virta <raine.virta@gmail.com>
2015-09-29 22:10:07 +0300

    obj-destr: use fantasydo

--- src/obj-destr.js
+++ src/obj-destr.js
@@ -4,28 +4,27 @@ const spliceStr = curryN(4, require('splice-string'));
 const fmt1 = curryN(2, require('util').format);
 const S = require('sanctuary');
 const { Just, Maybe } = S;
+const $do = require('fantasydo');
 
 const operate = curry((op, name, line) =>
-  S.fromMaybe(line,
-    S.indexOf('{', line).chain(braceStart =>
-      S.indexOf('}', line).chain(braceEnd => {
-        const removeAfterStart = braceEnd - braceStart + 1;
-        const insideBraces = slice(braceStart + 1, braceEnd, line);
-        const hasSpaceFirst = isSpace(head(insideBraces));
+  S.fromMaybe(line, $do(function* () {
+    const braceStart = yield S.indexOf('{', line);
+    const braceEnd   = yield S.indexOf('}', line);
+    const removeAfterStart = braceEnd - braceStart + 1;
+    const insideBraces = slice(braceStart + 1, braceEnd, line);
+    const hasSpaceFirst = isSpace(head(insideBraces));
 
-        return S.match(/[a-zA-Z0-9_]+/g, insideBraces)
-          .chain(commute(Just))
-          .concat(Just([]))
-          .map(names =>
-            pipe(
-              op(__, names),
-              join(', '),
-              fmt1(hasSpaceFirst ? '{ %s }' : '{%s}'),
-              spliceStr(line, braceStart, removeAfterStart)
-            )(name));
-      })
-    )
-  )
+    return yield S.match(/[a-zA-Z0-9_]+/g, insideBraces)
+      .chain(commute(Just))
+      .concat(Just([]))
+      .map(names =>
+        pipe(
+          op(__, names),
+          join(', '),
+          fmt1(hasSpaceFirst ? '{ %s }' : '{%s}'),
+          spliceStr(line, braceStart, removeAfterStart)
+        )(name));
+  }, Maybe))
 );
 
 //    without :: a -> [a] -> [a]
